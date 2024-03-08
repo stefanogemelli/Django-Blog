1eedb6de13a2c70dd177a53d4934523bcbb5010c
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-03-08 00:10:25 -0300

    update & install user models & makemigrations

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -88,6 +89,8 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT")
     }
 }
+
+AUTH_USER_MODEL = "user.User"
 
 
 # Password validation
