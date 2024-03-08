69145849169ab65053910b569359415979f7c55f
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-03-08 01:18:52 -0300

    update & migrate user.Profile model

--- blog/user/models.py
+++ blog/user/models.py
@@ -5,6 +5,12 @@ class User(AbstractUser):
     full_name = models.CharField(max_length=255)
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     photo = models.ImageField(blank=True, null=True)
+    profession = models.CharField(max_length=50, null=True)
+    about = models.TextField(null=True)
+    birthdate = models.DateTimeField(null=True)
+    twitter = models.URLField(max_length=50, null=True)
+    linkedin = models.URLField(max_length=50, null=True)
+    facebook = models.URLField(max_length=50, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
 
