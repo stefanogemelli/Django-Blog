d5c4d725882e8100f07d6e2fcd0cffb79145ddd1
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-03-11 06:16:05 -0300

    fix: url

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("user.urls")),
-    path("manage_post/", include("manage_post.urls")),
+    path("", include("manage_post.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
