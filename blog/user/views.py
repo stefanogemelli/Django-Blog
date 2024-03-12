99c4ae684b2ceddd0cd9f359f6de448d67dbf1f4
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-03-11 21:16:07 -0300

    feat: auto login

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,11 +1,36 @@
 from django.views.generic import CreateView
-from user.forms import SignUpForm, LoginForm
 from django.contrib.auth.views import LoginView
+from user.forms import SignUpForm, LoginForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+
+from django.contrib.auth import logout
+
+
+def logout_view(request):
+    logout(request)
+    # Redirect to a success page.
+    return redirect("login")
 
 
 class SignUpView(CreateView):
     form_class = SignUpForm
     template_name = "login/register.html"
+
+    def form_valid(self, form):
+        #Si es válido, guardar
+        form.save()
+
+        #Obtener username y password
+        user = authenticate(
+            username=form.cleaned_data["username"],
+            password=form.cleaned_data["password1"],
+        )
+
+        #Solicitud de inicio de sesión
+        login(self.request, user)
+        return redirect("login")
+
 
 class LoginView(LoginView):
     form_class = LoginForm
