857246a3f7b9bc53ca3a2e04db7a14dd02eb50fb
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-03-19 17:27:13 -0300

    perf: improve admin site

--- blog/manage_post/admin.py
+++ blog/manage_post/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 from manage_post.models import Category, Article, Rating
+from user.models import User
 
 
-admin.site.register(Category)
-admin.site.register(Article)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+    #Devolver articulos relacionados con el usuario
+    def get_queryset(self, request):
+        if request.user.is_superuser:
+            return Article.objects.all()
+        else:
+            return Article.objects.filter(user_id=request.user)
+    
+    #Devolver nombre de usuario relacionado con el usuario autenticado
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if (request.user.is_superuser) and (db_field.name == "user_id"):
+            kwargs['queryset'] = User.objects.filter(is_staff=1)
+        else:
+            kwargs['queryset'] = User.objects.filter(id=request.user.id)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Rating)
