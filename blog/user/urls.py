dcca4a6c24bfd75ebe9a2cdbd6a5383432b0cf62
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-03-19 09:41:32 -0300

    add: reset_password templates

--- blog/user/urls.py
+++ blog/user/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from user import views
 
 urlpatterns = [
@@ -9,6 +10,17 @@ urlpatterns = [
     path("delete/", views.UserDeleteView.as_view(), name="delete_user"),
     path("password/", views.PasswordsChangeView.as_view(), name="change_password"),
     path('profile/<int:pk>',views.ViewProfile.as_view(),name='view_profile'),
-    #provisional para que no rompa el template
-    path("passwordreset/", views.LoginView.as_view(), name="password_reset"),
+
+    path("password_reset/", PasswordResetView.as_view(
+        template_name = "password/password_reset.html"
+    ), name="password_reset"),
+    path("password_reset/done/", PasswordResetDoneView.as_view(
+        template_name="password/password_reset_done.html"
+    ), name="password_reset_done"),
+    path("reset/<uidb64>/<token>", PasswordResetConfirmView.as_view(
+        template_name="password/password_reset_confirm.html"
+    ), name="password_reset_confirm"),
+    path("reset/done/", PasswordResetCompleteView.as_view(
+        template_name="password/password_reset_complete.html"
+    ), name="password_reset_complete"),
 ]
